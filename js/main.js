e081e3bb94779326428bc03eb1fc49de8a0cabbf
Hiroki Chiyoda <contact@arafipro.com>
2023-06-20 12:19:41 +0900

    アプリ用プライバシーポリシーを作成

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 const menuBtn = document.getElementById("menu_btn");
 const navMenu = document.getElementById("nav_menu");
 
-menuBtn.addEventListener("click", () => {
-	navMenu.classList.toggle("nav_menu");
-	// this.style.display = "none";
+// // console.log(navmenu);
+window.addEventListener("scroll", function () {
+  const scrollupButton = document.getElementById("scrollup");
+  if (document.documentElement.scrollTop > 0 || document.body.scrollTop > 0) {
+    scrollupButton.style.display = "block";
+  } else {
+    scrollupButton.style.display = "none";
+  }
 });
 
-// console.log(navmenu);
+menuBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("nav_menu");
+  this.style.display = "none";
+});
