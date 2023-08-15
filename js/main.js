ceebd92d535640da360a370628ef03a251f9a51a
Hiroki Chiyoda <contact@arafipro.com>
2023-08-15 15:22:07 +0900

    レスポンシブナビゲーションメニューを作成

--- js/main.js
+++ js/main.js
@@ -1,32 +1,29 @@
-const header = document.getElementById("header");
 const scroll = document.getElementById("scroll");
-const headMenuBtn = document.getElementById("head_menu_btn");
-const heroMenuBtn = document.getElementById("hero_menu_btn");
-const navMenu = document.getElementsByClassName("nav_menu");
+const header = document.getElementById("header_main");
+const menuBtn = document.getElementById("menu_icon");
+const closeBtn = document.getElementById("close_icon");
+const navMenu = document.getElementById("nav_menu");
+const scrollupBtn = document.getElementById("scrollup");
 
-// ヘッダーメニュー固定
 const obs = new IntersectionObserver(
-  (entries) => {
+	(entries) => {
+		// ヘッダーメニューの固定
     header.classList.toggle("sticky", !entries[0].isIntersecting);
+		// スクロールアップボタンの表示
+    scrollupBtn.classList.toggle("active", !entries[0].isIntersecting);
   },
-  { rootMargin: "1100px 0px 0px 0px" }
+  { rootMargin: "1000px 0px 0px 0px" }
 );
 obs.observe(scroll);
 
-window.addEventListener("scroll", function () {
-  const scrollupButton = document.getElementById("scrollup");
-  if (document.documentElement.scrollTop > 0 || document.body.scrollTop > 0) {
-    scrollupButton.style.display = "block";
-  } else {
-    scrollupButton.style.display = "none";
-  }
+menuBtn.addEventListener("click", () => {
+  navMenu.classList.add("active");
+	menuBtn.style.display = "none"
+	closeBtn.style.display = "block"
 });
 
-headMenuBtn.addEventListener("click", () => {
-	console.log("clicked");
-	navMenu.classList.toggle("nav-active");
-});
-
-heroMenuBtn.addEventListener("click", () => {
-  console.log("clicked");
+closeBtn.addEventListener("click", () => {
+  navMenu.classList.remove("active");
+	menuBtn.style.display = "block"
+	closeBtn.style.display = "none"
 });
