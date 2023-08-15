c1b3559973c6e68cc9c39ea331c098fa77405e57
Hiroki Chiyoda <contact@arafipro.com>
2023-08-15 16:38:47 +0900

    ナビゲーションメニューの仕様追加
    ナビゲーションメニューのリンクをクリックしたらメニューを非表示にするように仕様変更

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ const navMenu = document.getElementById("nav_menu");
 const scrollupBtn = document.getElementById("scrollup");
 
 const obs = new IntersectionObserver(
-	(entries) => {
-		// ヘッダーメニューの固定
+  (entries) => {
+    // ヘッダーメニューの固定
     header.classList.toggle("sticky", !entries[0].isIntersecting);
-		// スクロールアップボタンの表示
+    // スクロールアップボタンの表示
     scrollupBtn.classList.toggle("active", !entries[0].isIntersecting);
   },
   { rootMargin: "1000px 0px 0px 0px" }
@@ -18,12 +18,20 @@ obs.observe(scroll);
 
 menuBtn.addEventListener("click", () => {
   navMenu.classList.add("active");
-	menuBtn.style.display = "none"
-	closeBtn.style.display = "block"
+  menuBtn.style.display = "none";
+  closeBtn.style.display = "block";
 });
 
-closeBtn.addEventListener("click", () => {
+const navLink = document.querySelectorAll(".nav_link");
+
+function linkAction() {
   navMenu.classList.remove("active");
-	menuBtn.style.display = "block"
-	closeBtn.style.display = "none"
+  menuBtn.style.display = "block";
+  closeBtn.style.display = "none";
+}
+
+closeBtn.addEventListener("click", linkAction);
+
+navLink.forEach((n) => {
+  n.addEventListener("click", linkAction);
 });
